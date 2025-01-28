a0c3cb6f539cb1336ce86504e7a52650a6c92b4c
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-01-29 00:12:34 +0200

    Add files via upload

---
+++ EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BookHaven.Web.Settings
+{
+	public class EmailSettings
+	{
+		public string? Email { get; set; }
+		public string? Passowrd { get; set; }
+		public string? DisplayName { get; set; }
+		public string? Host { get; set; }
+		public int port { get; set; }
+	}
+}
